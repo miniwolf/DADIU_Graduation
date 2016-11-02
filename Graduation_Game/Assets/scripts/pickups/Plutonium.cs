4aee5ed4efc0969f0050f85bdfeeba7945fab4f3
Alyn <alyn_509@yahoo.com>
2016-11-02 14:22:34 +0100

    inventory implemented & in progress pickups

---
+++ Graduation_Game/Assets/scripts/pickups/Plutonium.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.scripts;
+using Assets.scripts.components;
+using Assets.scripts.components.registers;
+using Assets.scripts.controllers;
+using Assets.scripts.controllers.handlers;
+
+public class Plutonium : MonoBehaviour {
+
+	Inventory inventory;
+	Dictionary<PickupActions, Handler> actions = new Dictionary<PickupActions, Handler>();
+
+	void Awake(){
+		inventory = GetComponent<Inventory> ();
+	}
+
+	public void OnTriggerEnter(Collider colider) {
+		if (colider.tag == TagConstants.PLAYER) {
+			inventory.plutonium.IncreaseAmount ();
+			//ExecuteAction(PickupActions.Despawn);
+		}
+		
+	}
+}
