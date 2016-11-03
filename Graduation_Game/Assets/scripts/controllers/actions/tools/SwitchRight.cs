3150aba8eb724a4223cafe39ea31541cb45815f2
Laura <lauryxky@hotmail.com>
2016-11-03 11:42:58 +0100

    Switch lane functionality implemented

--- Graduation_Game/Assets/scripts/controllers/actions/tools/SwitchRight.cs
+++ Graduation_Game/Assets/scripts/controllers/actions/tools/SwitchRight.cs
@@ -6,19 +6,43 @@ using Assets.scripts.components;
 namespace Assets.scripts.controllers.actions.tools {
 	public class SwitchRight : Action {
 		private readonly Directionable direction;
-		private Penguin penguin;
+		private GameObject penguin;
+		private MonoBehaviour couroutineHandler;
+		public float laneWidth = 2.0f;
 
 		public SwitchRight(Directionable direction){
 			this.direction = direction;
+
 		}
 
 		public void Setup(GameObject gameObject) {
-			penguin = gameObject.GetComponent<Penguin>();
+			penguin = gameObject;
+			couroutineHandler = gameObject.GetComponent<MonoBehaviour>();
 		}
 
-		public void Execute() {			
-			// change direction of the penguin 45 degrees to the right
-			penguin.SetDirection(Quaternion.Euler(0, 0, 45) * direction.GetDirection());
+		public void Execute() {		
+			Vector3 oldDirection = direction.GetDirection();
+			var oldRotation = penguin.transform.rotation;
+			var newRotation = Quaternion.Euler(0, 45, 0);
+			Vector3 newDirection = newRotation * direction.GetDirection();
+			// make sure that penguin can change lane
+			if ( !Physics.Raycast(penguin.transform.position, newDirection, laneWidth) ) {
+				direction.SetDirection(newDirection); //change penguin's direction
+				penguin.transform.rotation = newRotation; //rotate penguin
+				float zPos = penguin.transform.position.z;
+				couroutineHandler.StartCoroutine(LaneReached(zPos, oldDirection, oldRotation));
+			}
+		}
+
+		IEnumerator LaneReached(float zPos, Vector3 oldDirection, Quaternion oldRotation) {
+			do {
+				// check every 0.25 seconds if penguin has reached the half of the lane
+				yield return new WaitForSeconds(0.25f);
+			} while(penguin.transform.position.z > zPos - laneWidth);
+
+			//when half lane reached, change direction to old one
+			direction.SetDirection(oldDirection); 
+			penguin.transform.rotation = oldRotation;
 		}
 	}
 }
