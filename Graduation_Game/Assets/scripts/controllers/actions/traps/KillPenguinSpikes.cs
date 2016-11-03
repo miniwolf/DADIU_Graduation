1e4272b8a11a2b05c1eeb29d6e78bd43faa602cc
blacou12 <blacou12@student.aau.dk>
2016-11-03 12:45:36 +0100

    Spike trap and Pit trap

---
+++ Graduation_Game/Assets/scripts/controllers/actions/traps/KillPenguinSpikes.cs
@@ -0,0 +1,19 @@
+﻿using Assets.scripts.components;
+using UnityEngine;
+
+namespace Assets.scripts.controllers.actions.traps
+{
+	public class KillPenguinSpikes : Action {
+		private GameObject gameObject;
+
+		public void Setup(GameObject gameObject) {
+			this.gameObject = gameObject;
+		}
+
+		public void Execute() {
+			gameObject.GetComponentInChildren<Animator>().SetTrigger(AnimationConstants.SPIKEDEATH);
+			//Play sound
+			//Add all the global things here, e.g. remove a penguin from the amount you have, etc.
+		}
+	}
+}
