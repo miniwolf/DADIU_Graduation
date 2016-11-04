95bdaf639c6b503f6fe7cd1c29cdb85394dfa56c
Nicolai Hansen <nelkjaerhansen@gmail.com>
2016-11-04 10:34:03 +0100

    Made placeholder sounds for penguin, ambience and music
    Made following prefabs: UI sounds, music, ambience

---
+++ Graduation_Game/Assets/scripts/Audio anim triggers/PenguinSounds.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PenguinSounds : MonoBehaviour {
+
+	public void playMovingSound () {
+		AkSoundEngine.PostEvent ("penguin_move", gameObject);
+		Debug.Log ("moving sound is playing here from " + transform.parent.name);
+	}
+}
