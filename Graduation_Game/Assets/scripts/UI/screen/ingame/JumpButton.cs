05d2db8a35ad2a51e2245d4ec95efb81ed70581f
Laura <lauryxky@hotmail.com>
2016-11-04 14:02:23 +0100

    Jump tool added in the UI

---
+++ Graduation_Game/Assets/scripts/UI/screen/ingame/JumpButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using Asset.scripts.tools;
+using UnityEngine;
+
+namespace Assets.scripts.UI.screen.ingame {
+	public class JumpButton : MonoBehaviour {
+
+		private GameObject jumpObjTool;
+		private bool dragging;
+
+		public void PlaceJump() {
+			dragging = true;
+			GameObject jumpObj = GameObject.FindGameObjectWithTag(TagConstants.JUMP);
+			jumpObjTool = Instantiate(jumpObj); 
+		}
+
+		void Update() {
+			if ( Input.GetMouseButton(0) ) {
+				if ( dragging ) {
+					Ray ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+					RaycastHit hit;
+
+					if ( Physics.Raycast(ray, out hit) ) {
+						if ( hit.transform.tag.Equals(TagConstants.LANE) ) {
+							jumpObjTool.transform.position = hit.point;
+						}
+					}
+				}
+			}
+			// release object on scene
+			if ( Input.GetMouseButtonUp(0) ) {
+				// TODO: SNAP in lane
+				dragging = false;
+			}
+		}
+	}
+}
