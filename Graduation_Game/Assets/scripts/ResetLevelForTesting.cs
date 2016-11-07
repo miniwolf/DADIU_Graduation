87dbe3e51a082b5cb2eaddfc87c516d093838109
blacou12 <blacou12@student.aau.dk>
2016-11-07 11:25:56 +0100

    Added reset button for testing

---
+++ Graduation_Game/Assets/scripts/ResetLevelForTesting.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ResetLevelForTesting : MonoBehaviour {
+	public void ResetLevel(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
