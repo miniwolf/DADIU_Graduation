61fc6cd3751437198caa71b692bf30f3e8d6d147
Martin <martinvano00@gmail.com>
2016-11-09 10:04:12 +0100

    Generating weird slope

--- Graduation_Game/Assets/scripts/tools/slope/SlopeGenerator.cs
+++ Graduation_Game/Assets/scripts/tools/slope/SlopeGenerator.cs
@@ -9,22 +9,13 @@ public class SlopeGenerator : MonoBehaviour
 {
     public AnimationCurve slopeCurve;
 
-    private int width = 10, height = 10;
-    private int vertPerLine = 10;
-
+    private Vector3 scale = Vector3.one / 3;
+    private int vertPerLine = 1;
     // Use this for initialization
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
 
     public void Generate()
     {
-        MeshRenderer [] existing = GetComponentsInChildren<MeshRenderer>();
+        MeshRenderer[] existing = GetComponentsInChildren<MeshRenderer>();
 
         foreach (MeshRenderer t in existing)
         {
@@ -69,28 +60,71 @@ public class SlopeGenerator : MonoBehaviour
 //        meshFilter.sharedMesh = mesh;
 
 
-         for (int x = 0; x < length; x++)
-         {
-             float pos = x / (float) length;
-             float y = length * slopeCurve.Evaluate(pos);
+        var data = new MeshData(length, 2);
+        int vertexIndex = 0;
 
-             Vector3 start  = new Vector3(x,y , 0);
-             Vector3 end = new Vector3(x + 1, length * slopeCurve.Evaluate((x + 1) / (float) length), 0f);
+        float minY = float.MaxValue;
+        float startX = 0, endX = length;
 
-             {
-                 GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                 GameObject cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                 cube1.transform.position = start;
-                 cube1.transform.localScale = Vector3.one / 3;
+        for (int x = 0; x < length; x++) // generate curve
+        {
+            float pos = x / (float) length;
+            float y = length * slopeCurve.Evaluate(pos);
 
-                 start.z = start.z + width;
-                 cube2.transform.position = start;
-                 cube2.transform.localScale = Vector3.one / 3;
+            if (y < minY)
+                minY = y;
 
-                 cube1.transform.parent = transform;
-                 cube2.transform.parent = transform;
-             }
-         }
+            Vector3 start = new Vector3(x, y, 0);
+//            Vector3 end = new Vector3(x + 1, length * slopeCurve.Evaluate((x + 1) / (float) length), 0f);
+            {
+                PlaceCube(start);
+                data.vertices[vertexIndex] = start;
+                data.uvs[vertexIndex] = new Vector2(x / (float) length, 0);
+                data.AddTriangle(vertexIndex, vertexIndex + vertPerLine + 1, vertexIndex + vertPerLine);
+                data.AddTriangle(vertexIndex + vertPerLine + 1, vertexIndex, vertexIndex + 1);
+                vertexIndex++;
+
+                start.z = start.z + width;
+
+                PlaceCube(start);
+                data.vertices[vertexIndex] = start;
+                data.uvs[vertexIndex] = new Vector2(x / (float) length, 1);
+                data.AddTriangle(vertexIndex, vertexIndex + vertPerLine + 1, vertexIndex + vertPerLine);
+                data.AddTriangle(vertexIndex + vertPerLine + 1, vertexIndex, vertexIndex + 1);
+                vertexIndex++;
+            }
+
+            var mesh = new Mesh();
+            mesh.vertices = data.vertices;
+            mesh.triangles = data.triangles;
+            mesh.uv = data.uvs;
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+
+            GameObject slope = new GameObject();
+            slope.transform.parent = transform;
+            MeshFilter meshFilter = slope.AddComponent<MeshFilter>();
+            MeshRenderer renderer = slope.AddComponent<MeshRenderer>();
+            renderer.material.color = Color.white;
+            meshFilter.sharedMesh = mesh;
+        }
+
+
+        for (int x = 0; x < length; x++) // generate bottoms
+        {
+            PlaceCube(new Vector3(x, minY, 0));
+            PlaceCube(new Vector3(x, minY, width));
+//            PlaceCube(new Vector3(endX, minY, 0));
+//            PlaceCube(new Vector3(endX, minY, width));
+        }
+    }
+
+    private void PlaceCube(Vector3 vector3)
+    {
+        GameObject corner = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        corner.transform.position = vector3;
+        corner.transform.localScale = scale;
+        corner.transform.parent = transform;
     }
 
     public class MeshData
