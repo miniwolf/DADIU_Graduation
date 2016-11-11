fd7e4b59172ae1ee3f82663195c6d0d7717785e5
blacou12 <blacou12@student.aau.dk>
2016-11-11 12:51:36 +0100

    excavator fix, displaces 2 from origin pos, will not go down on small penguins

--- Graduation_Game/Assets/scripts/traps/ExcavatorTrap.cs
+++ Graduation_Game/Assets/scripts/traps/ExcavatorTrap.cs
@@ -5,12 +5,13 @@ using Assets.scripts.controllers;
 
 // TODO add conditions: 
 // Minimize and metal penguin tools does not active this trap
+using Assets.scripts.character;
 
 namespace Assets.scripts.traps {
 	// Note: This trap uses a trigger and a collider seperately
 
 	public class ExcavatorTrap : MonoBehaviour {
-		public float endDist = 0.5f;
+		public float displacement = 2f;
 		public float resetAfterSeconds = 1.5f;
 		private float initialPosY;
 
@@ -24,6 +25,9 @@ namespace Assets.scripts.traps {
 		// The trigger event handles the activation event of the excavator
 		protected IEnumerator OnTriggerEnter(Collider other) {
 			if (other.transform.tag == TagConstants.PENGUIN) {
+				if (other.GetComponent<Penguin>().GetWeight()==Penguin.Weight.Small) {
+					return false;
+				}
 
 				ExecuteExcavator();
 
@@ -43,7 +47,7 @@ namespace Assets.scripts.traps {
 
 		// Moves the excavator down by endDist
 		private void ExecuteExcavator() {
-			transform.position = new Vector3(initialPosition.x, endDist, initialPosition.z);
+			transform.position = new Vector3(initialPosition.x, initialPosition.y-displacement, initialPosition.z);
 		}
 
 		// Resets the excavator to its initial position and rotation
