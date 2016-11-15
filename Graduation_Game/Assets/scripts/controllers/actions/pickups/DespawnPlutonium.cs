5d7ab48d1175faede26d052ddeaf475cce3aef75
Martin <martinvano00@gmail.com>
2016-11-15 16:46:48 +0100

    Feedback working without particles

--- Graduation_Game/Assets/scripts/controllers/actions/pickups/DespawnPlutonium.cs
+++ Graduation_Game/Assets/scripts/controllers/actions/pickups/DespawnPlutonium.cs
@@ -3,58 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using Assets.scripts.components;
 using UnityEngine;
+using UnityEngine.Assertions.Comparers;
 using UnityEngine.UI;
 
-namespace Assets.scripts.controllers.actions.pickups
-{
-    public class DespawnPlutonium : Action
-    {
-        private GameObject gameObject;
-        private Text plutoniumCounter;
-        private readonly CouroutineDelegateHandler delegator;
+namespace Assets.scripts.controllers.actions.pickups {
+	public class DespawnPlutonium : Action {
+		private GameObject gameObject;
+		private Text plutoniumCounter;
+		private readonly CouroutineDelegateHandler delegator;
 
-        public DespawnPlutonium(CouroutineDelegateHandler d)
-        {
-            delegator = d;
-        }
+		public DespawnPlutonium(CouroutineDelegateHandler d) {
+			delegator = d;
+		}
 
-        public void Setup(GameObject gameObject)
-        {
-            this.gameObject = gameObject;
-            plutoniumCounter =
-                GameObject.FindGameObjectWithTag(TagConstants.PLUTONIUM_COUNTER_TEXT).GetComponent<Text>();
-        }
+		public void Setup(GameObject gameObject) {
+			this.gameObject = gameObject;
+			plutoniumCounter = GameObject.FindGameObjectWithTag(TagConstants.PLUTONIUM_COUNTER_TEXT).GetComponent<Text>();
+		}
 
-        public void Execute()
-        {
-            delegator.StartCoroutine(FeedbackCoroutine());
-        }
+		public void Execute() {
+			delegator.StartCoroutine(FeedbackCoroutine());
+		}
 
-        private IEnumerator FeedbackCoroutine()
-        {
-            Camera c = Camera.main;
-            GameObject currencyGameObject = gameObject.transform.parent.gameObject;
-            Vector3 counterCanvasPos = c.ScreenToViewportPoint(plutoniumCounter.transform.position);
-            Vector3 currencyCanvasPos = currencyGameObject.transform.position;
+		private IEnumerator FeedbackCoroutine() {
+			Camera c = Camera.main;
+			GameObject currencyGameObject = gameObject.transform.parent.gameObject;
+			Vector3 counterCanvasPos = c.ScreenToWorldPoint(plutoniumCounter.transform.position);
+			Vector3 currencyCanvasPos = currencyGameObject.transform.position;
 
-            float fraction = 0;
-            float speed = .1f;
+			counterCanvasPos.z = currencyCanvasPos.z; // we don't want to change the z-axis of item picked up
+			counterCanvasPos.x += 10; // magic number that fixes most of the issues
 
-			while (Vector3.Distance(currencyGameObject.transform.position, counterCanvasPos) > 1)
-            {
-                Debug.Log(counterCanvasPos + "|" + currencyCanvasPos);
-                if (fraction < 1)
-                {
-                    fraction += Time.deltaTime * speed;
-                    currencyGameObject.transform.position = Vector3.Lerp(currencyCanvasPos, counterCanvasPos, fraction);
-                }
+			float fraction = 0;
+			float speed = 1f;
+			float distance = float.MaxValue;
 
-                yield return new WaitForEndOfFrame();
-            }
+			while(distance > 1) {
+				if(fraction < 1) {
+					fraction += Time.deltaTime * speed;
+					currencyGameObject.transform.position = Vector3.Lerp(currencyCanvasPos, counterCanvasPos, fraction);
+				}
 
-//            currencyGameObject.SetActive(false);
-            plutoniumCounter.text = (int.Parse(plutoniumCounter.text) + 1).ToString();
-            yield return null;
-        }
-    }
+				distance = Vector3.Distance(currencyGameObject.transform.position, counterCanvasPos);
+				yield return new WaitForEndOfFrame();
+			}
+
+			currencyGameObject.SetActive(false);
+			plutoniumCounter.text = (int.Parse(plutoniumCounter.text) + 1).ToString();
+			yield return null;
+		}
+	}
 }
