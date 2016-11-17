4495924a57fffc001d23811de23e57acc58e1d0f
Martin <martinvano00@gmail.com>
2016-11-17 13:53:21 +0100

    Translations from the mini-production 2

---
+++ Graduation_Game/Assets/scripts/UI/KillPenguin.cs
@@ -0,0 +1,29 @@
+﻿using Assets.scripts.components;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.scripts.components.registers;
+
+namespace Assets.scripts.controllers.actions.traps {
+	public class KillPenguin : Action {
+		private readonly Killable killable;
+	    private readonly NotifierSystem notifierSystem;
+	    private GameObject penguin;
+
+	    public KillPenguin(Killable killable, NotifierSystem s) {
+			this.killable = killable;
+		    this.notifierSystem = s;
+		}
+
+		public void Setup(GameObject gameObject) {
+			penguin = gameObject;
+		}
+
+		public void Execute() {
+			var penguinCounter = GameObject.FindGameObjectWithTag(TagConstants.PENGUIN_COUNTER_TEXT).GetComponent<Text>();
+			penguinCounter.text = (int.Parse(penguinCounter.text) - 1).ToString();
+			killable.Kill();
+			notifierSystem.PenguinDied(penguin);
+			penguin.GetComponent<Collider>().enabled = false;
+		}
+	}
+}
