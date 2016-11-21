da40877ee34a6adf13161793a6feaca34598a8a7
Nicklas 'MiNiWolF' Pingel <miniwolf1508@gmail.com>
2016-11-21 11:43:32 +0100

    Update script to make move being sounded

--- Graduation_Game/Assets/scripts/Audio anim triggers/PenguinSounds.cs
+++ Graduation_Game/Assets/scripts/Audio anim triggers/PenguinSounds.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PenguinSounds : MonoBehaviour {
 
 	public void playMovingSound () {
-		AkSoundEngine.PostEvent ("penguin_move1", gameObject);
+		AkSoundEngine.PostEvent ("penguin_move", gameObject);
 		//Debug.Log ("moving sound is playing here from " + transform.parent.name);
 	}
 }
