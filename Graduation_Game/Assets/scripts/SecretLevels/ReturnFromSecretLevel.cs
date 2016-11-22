501aa30211b7d66a2157e1ac353a88a3b605e687
blacou12 <blacou12@student.aau.dk>
2016-11-22 14:32:55 +0100

    Return from secret level

---
+++ Graduation_Game/Assets/scripts/SecretLevels/ReturnFromSecretLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.scripts;
+using UnityEngine.SceneManagement;
+
+public class ReturnFromSecretLevel : MonoBehaviour {
+	void OnTriggerEnter(Collider other){
+		if (other.tag != TagConstants.PENGUIN) {
+			SceneManager.LoadScene(PlayerPrefs.GetString("thisCurrLvl"));
+		}
+	}
+}
