b9679bf059fa241298884bd9e1e68ea487228e22
Alyn <alyn_509@yahoo.com>
2016-11-23 16:08:35 +0100

    core update by curve

--- Graduation_Game/Assets/scripts/UI/endScreen/Star.cs
+++ Graduation_Game/Assets/scripts/UI/endScreen/Star.cs
@@ -5,10 +5,6 @@ using UnityEngine.UI;
 
 namespace Assets.scripts.tools {
 	public class Star : ActionableGameEntityImpl<GameActions> {
-		public AnimationCurve curve;
-		public Vector3 startPosition;
-		public Vector3 endPosition;
-
 		public void FlyIn() {
 			gameObject.GetComponent<Image>().enabled = true;
 			//ExecuteAction(GameActions.TriggerStar);
