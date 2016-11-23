e4f64175037068c5bff16a288c09f2e93cba9429
Alyn <alyn_509@yahoo.com>
2016-11-23 13:44:52 +0100

    EndScene partial Refactoring

--- Graduation_Game/Assets/scripts/controllers/actions/game/EndGame.cs
+++ Graduation_Game/Assets/scripts/controllers/actions/game/EndGame.cs
@@ -14,7 +14,7 @@ namespace Assets.scripts.controllers.actions.game {
 		private GameObject endScene;
 		private Text plutoniumCounter;
 		private Text plutoniumThisLevel;
-		private GameObject plutoniumTotal;
+		private Text plutoniumTotal;
 		private GameObject penguinCounter;
 		private GameObject[] star = new GameObject[3];
 		private bool isSetUp = false;
@@ -22,15 +22,13 @@ namespace Assets.scripts.controllers.actions.game {
 		private int starsSpawned;
 		private bool scoreUpdated = false;
 		private readonly CouroutineDelegateHandler handler;
-		private PlutoniumCounterController pcc;
 		private SceneManager scenes;
 		private Actionable<GameActions> actionable;
 
 		public void Setup(GameObject gameObject) {
 			this.gameObject = gameObject;
 			canvas = gameObject.GetComponent<CanvasController>();
-			penguinCounter = GameObject.FindGameObjectWithTag(TagConstants.PENGUIN_COUNTER_TEXT);
-			plutoniumTotal = GameObject.FindGameObjectWithTag(TagConstants.PLUTONIUM_TOTAL);
+			plutoniumTotal = GameObject.FindGameObjectWithTag(TagConstants.PLUTONIUM_TOTAL).GetComponent<Text>();
 			endScene = GameObject.FindGameObjectWithTag(TagConstants.ENDSCENE);
 			star[0] = GameObject.FindGameObjectWithTag(TagConstants.STAR1);
 			star[1] = GameObject.FindGameObjectWithTag(TagConstants.STAR2);
@@ -53,33 +51,30 @@ namespace Assets.scripts.controllers.actions.game {
 			}
 
 			if (!scoreUpdated) {
-				handler.StartCoroutine(UpdateScore());
+				handler.StartCoroutine(EndLevel());
 			}
 		}
 
 		private void SetupEndScene()
 		{
 		    AkSoundEngine.PostEvent(SoundConstants.FeedbackSounds.END_SCREEN_TRIGGER, Camera.main.gameObject);
-			plutoniumThisLevel.text = plutoniumCounter.text;
+			plutoniumThisLevel.GetComponent<Text>().text = plutoniumCounter.text;
 
-			endScene.GetComponent<Image>().enabled = true;
-			plutoniumTotal.GetComponent<Text>().enabled = true;
-			plutoniumThisLevel.GetComponent<Text>().enabled = true;
+			endScene.SetActive(true);
 			plutoniumTotal.GetComponent<Text>().text = PlayerPrefs.GetInt("Plutonium").ToString();
 
 			target = PlayerPrefs.GetInt("Plutonium") + int.Parse(plutoniumThisLevel.text);
-			pcc = plutoniumThisLevel.GetComponent<PlutoniumCounterController>();
-			pcc.SetupFlowing();
 			isSetUp = true;
 		}
 
-		private IEnumerator UpdateScore() {
+		private IEnumerator EndLevel() {
 			if (!PlayerPrefs.HasKey("Plutonium")) {
 				PlayerPrefs.SetInt("Plutonium", 0);
 				PlayerPrefs.Save();
 			}
-			while (pcc.FlowPlutonium()) {
-				yield return new WaitForSeconds(0.1f);
+			while (int.Parse(plutoniumThisLevel.text) > 0) {
+				UpdateScore(1);
+				yield return new WaitForSeconds(0.01f);
 			}
 			while (target != int.Parse(plutoniumTotal.GetComponent<Text>().text)) {
 				yield return new WaitForSeconds(0.5f);
@@ -113,7 +108,12 @@ namespace Assets.scripts.controllers.actions.game {
 			PlayerPrefs.DeleteKey("hasVisited");
 			yield return null;
 		}
-		
+
+		private void UpdateScore(int portion) {
+			plutoniumTotal.text = (int.Parse(plutoniumTotal.text) + portion).ToString();
+			plutoniumThisLevel.text = (int.Parse(plutoniumThisLevel.text) - portion).ToString();
+		}
+
 		public bool SpawnNextStar() {
 			for (int i = 0; i < 3; i++) {
 				if (starsSpawned == i) {
--- Graduation_Game/Assets/scripts/controllers/actions/pickups/DespawnPlutonium.cs
+++ Graduation_Game/Assets/scripts/controllers/actions/pickups/DespawnPlutonium.cs
@@ -8,19 +8,16 @@ namespace Assets.scripts.controllers.actions.pickups {
 	public class DespawnPlutonium : Action {
 		private GameObject gameObject;
 		private Text currencyCounter;
-		private Text totalCounter;
 		private int pointsToAdd;
 		private Text plutoniumCounter;
-		private Text plutoniumThisLevel;
 		private Image counter; 
 		private RectTransform canvas;
 		private readonly CouroutineDelegateHandler delegator;
 		private Actionable<PickupActions> actionable;
 
-		public DespawnPlutonium(CouroutineDelegateHandler d, Text counter, Actionable<PickupActions> actionable, Text total = null) {
+		public DespawnPlutonium(CouroutineDelegateHandler d, Text counter, Actionable<PickupActions> actionable) {
 			delegator = d;
 			currencyCounter = counter; // GameObject.FindGameObjectWithTag(TagConstants.PLUTONIUM_COUNTER_TEXT).GetComponent<Text>();
-			totalCounter = total;
 			this.actionable = actionable;
 		}
 
@@ -28,7 +25,6 @@ namespace Assets.scripts.controllers.actions.pickups {
 			this.gameObject = gameObject;
 			pointsToAdd = 0;
 			plutoniumCounter = GameObject.FindGameObjectWithTag(TagConstants.PLUTONIUM_COUNTER_TEXT).GetComponent<Text>();
-			plutoniumThisLevel = GameObject.FindGameObjectWithTag(TagConstants.PLUTONIUM_THIS_LEVEL).GetComponent<Text>();
 			counter = plutoniumCounter.transform.parent.GetComponent<Image>();
 			canvas = GameObject.Find(TagConstants.CANVAS).GetComponent<RectTransform>();
 		}
@@ -44,23 +40,15 @@ namespace Assets.scripts.controllers.actions.pickups {
 			Vector3 worldPos;
 			GameObject currencyGameObject = gameObject.transform.parent.gameObject;
 			Vector3 counterCanvasPos;
-			if (totalCounter == null) {
-				counterCanvasPos = c.ScreenToWorldPoint(currencyCounter.transform.position);
-			}
-			else {
-				counterCanvasPos = c.ScreenToWorldPoint(totalCounter.transform.position);
-			}
+			counterCanvasPos = c.ScreenToWorldPoint(currencyCounter.transform.position);
 			RectTransformUtility.ScreenPointToWorldPointInRectangle(canvas,plutoniumCounter.rectTransform.position,c,out worldPos);
 			counterCanvasPos = worldPos;
 			Vector3 currencyCanvasPos = currencyGameObject.transform.position;
 
 			counterCanvasPos.z = currencyCanvasPos.z; // we don't want to change the z-axis of item picked up
-
-
-			if (totalCounter == null) {
-				ParticleSystem p = currencyGameObject.AddComponent<ParticleSystem>();
-				p.startSpeed = 10f;
-			}
+						
+			ParticleSystem p = currencyGameObject.AddComponent<ParticleSystem>();
+			p.startSpeed = 10f;
 
 			float fraction = 0;
 			float speed = 1f;
@@ -75,13 +63,6 @@ namespace Assets.scripts.controllers.actions.pickups {
 				distance = Vector3.Distance(currencyGameObject.transform.position, counterCanvasPos);
 				yield return new WaitForEndOfFrame();
 			}
-
-			if (totalCounter == null) {
-				gameObject.transform.parent.parent = plutoniumThisLevel.transform;
-			}
-			else {
-				gameObject.transform.parent.parent = totalCounter.transform;
-			}
 			
 			currencyGameObject.SetActive(false);
 			//plutoniumCounter.text = (int.Parse(plutoniumCounter.text) + 1).ToString();
@@ -95,13 +76,7 @@ namespace Assets.scripts.controllers.actions.pickups {
 		}
 
 		private void ApplyChange(int portion) {
-			if (totalCounter == null) {
-				currencyCounter.text = (int.Parse(currencyCounter.text) + portion).ToString();
-			}
-			else {
-				plutoniumThisLevel.text = (int.Parse(plutoniumThisLevel.text) - portion).ToString();
-				totalCounter.text = (int.Parse(totalCounter.text) + portion).ToString();
-			}
+			currencyCounter.text = (int.Parse(currencyCounter.text) + portion).ToString();
 			pointsToAdd -= portion;
 		}
 	}
