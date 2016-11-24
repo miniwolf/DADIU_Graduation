b2a792b909e4847e73c3bfbb6873615c2da0fa20
Alyn <alyn_509@yahoo.com>
2016-11-24 10:31:26 +0100

    buttons in end screen

---
+++ Graduation_Game/Assets/scripts/UI/endScreen/EndSceneController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndSceneController: MonoBehaviour {
+
+	public void GoToShop() {
+
+	}
+	public void Retry() {
+
+	}
+	public void BackToMenu() {
+
+	}
+	public void GoNextLevel() {
+
+	}
+}
