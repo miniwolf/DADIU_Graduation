d24257d4e5a9f5c1c0c0473acc1e225587010025
Alyn <alyn_509@yahoo.com>
2016-11-24 13:24:43 +0100

    buttons in End Scene

--- Graduation_Game/Assets/scripts/UI/endScreen/EndSceneController.cs
+++ Graduation_Game/Assets/scripts/UI/endScreen/EndSceneController.cs
@@ -1,18 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.scripts.UI.inventory;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using System;
+using System.IO;
 
 public class EndSceneController: MonoBehaviour {
 
 	public void GoToShop() {
-
+		SceneManager.LoadScene("Store");
 	}
 	public void Retry() {
-
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 	public void BackToMenu() {
-
+		SceneManager.LoadScene("MainMenuScene");
 	}
 	public void GoNextLevel() {
-
+		string nextLevel = Regex.Replace(SceneManager.GetActiveScene().name, @"[\d-]", string.Empty) + (int.Parse(Regex.Match(SceneManager.GetActiveScene().name, @"\d+").Value) + 1).ToString();
+		try {
+			SceneManager.LoadScene(nextLevel);
+		}
+		catch {
+			SceneManager.LoadScene("MainMenuScene");
+		}
 	}
 }
