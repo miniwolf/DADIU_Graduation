8bf1abd8f828a02a5888fac81d6f278a6989ae59
Alyn <alyn_509@yahoo.com>
2016-11-29 13:02:58 +0100

    AnimationSet Added

---
+++ Graduation_Game/Assets/scripts/AnimationSet.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.scripts {
+	public class AnimationSet {
+
+		public string fallAnimation;
+		public string jumpAnimation;
+		public string slidingAnimation;
+		public string celebrateAnimation;
+		public string deathElectricAnimation;
+		public string deathDrownAnimation;
+		public string deathPitAnimation;
+		public string deathSpikeAnimation;
+	}
+}
