be09626ddf9aa0d1f158133109893e70df1277cf
blacou12 <blacou12@student.aau.dk>
2016-12-02 14:46:30 +0100

    Transparent mesh is added under opacityOverall prefab, ask bo for explaining how it works

---
+++ Graduation_Game/Assets/scripts/opacity/OpacityTriggerEnter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.scripts;
+
+public class OpacityTriggerEnter : MonoBehaviour {
+
+	public GameObject[] toMakeTransparent;
+	public Material transparent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider col){
+		if (col.transform.tag != TagConstants.PENGUIN) {
+			return;
+		}
+		if (toMakeTransparent == null) {
+			Debug.LogError("No gameobjects added to be made transparent");
+			return;
+		}
+		for (int i = 0; i < toMakeTransparent.Length; i++) {
+			foreach (Transform t in toMakeTransparent[i].GetComponentInChildren<Transform>()) {
+				t.gameObject.GetComponent<MeshRenderer>().material = transparent;
+			}
+		}
+	}
+}
